5adc1bfa49b138f1ef966d38bfc91f8cfb9cee96
Aaron Suen <warr1024@gmail.com>
2012-12-22 10:53:10 -0500

    Another attempt to fix redstone->glowstone exploits.

--- src/BWRBlockRedstoneWire.java
+++ src/BWRBlockRedstoneWire.java
@@ -46,11 +46,6 @@ public class BWRBlockRedstoneWire extends BlockRedstoneWire
 		if(newmeta <= oldmeta)
 			return;
 
-		// Probability of continuing the effect is proportional to the change
-		// in signal strength.
-		if((newmeta - oldmeta) < world.rand.nextInt(128))
-			return;
-
 		// Make sure that we have a downward-facing lens immediately above the redstone
 		// wire to concentrate sunlight into it, and that we have the requisite sunlight.
 		if((y >= 255)
@@ -71,15 +66,41 @@ public class BWRBlockRedstoneWire extends BlockRedstoneWire
 			world.scheduleBlockUpdate(x, y, z, this.blockID, 9);
 		}
 
+	// Called when the block is added/placed.
+	public void onBlockAdded(World world, int x, int y, int z)
+		{
+		// Redstone, once placed, needs to "warm up" before the glowstone transformation
+		// can begin.  This discourages builds that rely on the fact that redstone can
+		// be placed/removed rapidly, obviating the need for a circuit to detect glowstone
+		// creation.  To do this, schedule a block update long after placement, which will
+		// block the scheduling of any further updates.
+		if(!world.IsUpdateScheduledForBlock(x, y, z, this.blockID))
+			world.scheduleBlockUpdate(x, y, z, this.blockID, 100);
+		}
+
 	// Delayed tick event, called by World, and schduled by onNeighborBlockChange.
 	public void updateTick(World world, int x, int y, int z, Random r)
 		{
+		// Probability of transformation is dependent on signal strength.
+		int meta = world.getBlockMetadata(x, y, z);
+		if(meta < world.rand.nextInt(256))
+			return;
+
+		// Repeat the environment checks, to make sure none of this
+		// has changed since the initial power injection.
+		if((y >= 255)
+			|| world.provider.hasNoSky
+			|| (world.getBlockId(x, y + 1, z) != mod_FCBetterThanWolves.fcLens.blockID)
+			|| (world.getBlockMetadata(x, y + 1, z) != 0)
+			|| !world.isDaytime()
+			|| world.isRaining()
+			|| !world.canBlockSeeTheSky(x, y + 2, z))
+			return;
+
 		// Play the effect of redstone wire being broken.
 		world.playAuxSFX(2001, x, y, z, this.blockID);
 
-		// Remove the existing redstone wire.  Save the old metadata
-		// value in case we have to replace it.
-		int meta = world.getBlockMetadata(x, y, z);
+		// Remove the existing redstone wire.
 		world.setBlockAndMetadataWithNotify(x, y, z, 0, 0);
 
 		// 10% of the time, pop glowstone dust off as an item.
